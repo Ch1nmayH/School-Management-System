3f02e5898e985df75eab8a2deff22164c3648531
Chinmay Hegde <cod1.1backup@gmail.com>
2024-11-20 20:04:08 +0530

    Installed bcrypt to hash password and added admin router.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import studentRouter from "./routes/studentRoute.js"
 import teacherRouter from "./routes/teacherRoute.js"
 import classRouter from "./routes/classRoute.js"
+import adminRouter from "./routes/adminRoute.js"
 
 import dbConnection from "./config/dbConnection.js"
 
@@ -22,6 +23,7 @@ app.get("/", (req , res)=>{
 app.use("/api/student", studentRouter);
 app.use("/api/teacher", teacherRouter);
 app.use("/api/class", classRouter);
+app.use("/api/admin", adminRouter);
 
 const port = process.env.PORT || 6000;
 

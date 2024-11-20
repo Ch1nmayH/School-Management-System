421c3101c1bc9581b810bb73632fcf87b32c1947
Chinmay Hegde <cod1.1backup@gmail.com>
2024-11-20 20:03:43 +0530

    refactored the middleware with try catch

--- middlewares/studentMiddleware.js
+++ middlewares/studentMiddleware.js
@@ -3,85 +3,85 @@ import Teacher from "../models/teacherModel.js";
 import Admin from "../models/adminModel.js";
 import jwt from "jsonwebtoken";
 
-const checkStudentAuth = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(200).json({ message: "You are not logged in." });
-  }
-  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-  if (!decodedData) {
-    return res.status(200).json({ message: "Invalid Token." });
-  }
+const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return null; 
+    }
+};
 
-  const studentExists = await Student.findById(decodedData._id);
-  if (!studentExists) {
-    const teacherExsits = await Teacher.findById(decodedData._id);
-    if (!teacherExsits) {
-      const adminExists = await Admin.findById(decodedData._id);
-      if (adminExists) {
-        if (!adminExists.isAdmin) {
-          return res.status(200).json({ message: "Unauthorised User." });
+const checkStudentAuth = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: "You are not logged in." });
+    }
+
+    const decodedData = verifyToken(token);
+    if (!decodedData) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
+
+    try {
+        const studentExists = await Student.findById(decodedData._id);
+        if (!studentExists) {
+            return res.status(403).json({ message: "Unauthorized user." });
         }
         next();
-      } else if (!adminExists) {
-        return res.status(200).json({ message: "Unauthorised User." });
-      }
+    } catch (err) {
+        res.status(500).json({ message: "Server error while verifying user." });
     }
-  }
-
-  next();
 };
 
 const checkTeacherAuth = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(200).json({ message: "You are not logged in." });
-  }
-  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-  if (!decodedData) {
-    return res.status(200).json({ message: "Invalid Token." });
-  }
-
-  const teacherExsits = await Teacher.findById(decodedData._id);
-  if (!teacherExsits) {
-    const adminExists = await Admin.findById(decodedData._id);
-    if (adminExists) {
-      if (!adminExists.isAdmin) {
-        return res.status(200).json({ message: "Unauthorised User." });
-      }
-      next();
-    } else if (!adminExists) {
-      return res.status(200).json({ message: "Unauthorised User." });
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: "You are not logged in." });
     }
-  }
 
-  next();
+    const decodedData = verifyToken(token);
+    if (!decodedData) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
+
+    try {
+        const teacherExists = await Teacher.findById(decodedData._id);
+        if (!teacherExists) {
+            const adminExists = await Admin.findById(decodedData._id);
+            if (!adminExists || !adminExists.isAdmin) {
+                return res.status(403).json({ message: "Unauthorized user." });
+            }
+        }
+        next();
+    } catch (err) {
+        res.status(500).json({ message: "Server error while verifying user." });
+    }
 };
 
 const checkAdminAuth = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(200).json({ message: "You are not logged in." });
-  }
-  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-  if (!decodedData) {
-    return res.status(200).json({ message: "Invalid Token." });
-  }
-
-  const adminExists = await Admin.findById(decodedData._id);
-  if (adminExists) {
-    if (!adminExists.isAdmin) {
-      return res.status(200).json({ message: "Unauthorised User." });
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: "You are not logged in." });
     }
-    next();
-  } else if (!adminExists) {
-    return res.status(200).json({ message: "Unauthorised User." });
-  }
-  next();
+
+    const decodedData = verifyToken(token);
+    if (!decodedData) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
+
+    try {
+        const adminExists = await Admin.findById(decodedData._id);
+        if (!adminExists || !adminExists.isAdmin) {
+            return res.status(403).json({ message: "Unauthorized user." });
+        }
+        next();
+    } catch (err) {
+        res.status(500).json({ message: "Server error while verifying user." });
+    }
 };
 
 export default {
-  checkStudentAuth,
-  checkTeacherAuth,
-  checkAdminAuth,
+    checkStudentAuth,
+    checkTeacherAuth,
+    checkAdminAuth,
 };
